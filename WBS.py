7af83f6a55d9e7e496f8537fb3dee6e358acf67c
fr18711 <sylvain.vuillemet@worldline.com>
2020-11-25 15:14:41 +0000

    WIP. Status between -1 and 100%. Introducing up and down row

--- WBS.py
+++ WBS.py
@@ -1,4 +1,5 @@
 import csv
+import copy
 import logging
 import argparse
 
@@ -9,14 +10,16 @@ class WbsGenerator() :
 <style>
 wbsDiagram {
   .group {
-      BackgroundColor grey
       RoundCorner 40
   }
   .backlog {
-      BackgroundColor yellow
+      BackgroundColor silver
   }
-  .running {
-      BackgroundColor orange
+  .backlogLate {
+      BackgroundColor red
+  }
+  .runningLate {
+      BackgroundColor red
   }
   .done {
       BackgroundColor green
@@ -55,6 +58,64 @@ wbsDiagram {
       self.nodeAsWbs(c)
 
 #============================================
+class GanttGenerator() :
+  head='''
+@startuml
+<style>
+wbsDiagram {
+  .group {
+      RoundCorner 40
+  }
+  .backlog {
+      BackgroundColor silver
+  }
+  .backlogLate {
+      BackgroundColor red
+  }
+  .runningLate {
+      BackgroundColor red
+  }
+  .done {
+      BackgroundColor green
+  }
+  .critical {
+    BackgroundColor orange
+    LineColor red
+    LineThickness 5.0
+  }
+  .neutral {
+      BackgroundColor white
+  }
+}
+</style>
+saturday are closed
+sunday are closed
+'''
+  tail="@enduml"
+
+  #----------------------------------------------------
+  def __init__(self,args,tree) :
+    self.tree=tree
+    self.lines=[]
+    self.treeToGantt()
+
+  #----------------------------------------------------
+  def treeToGantt(self) :
+    self.lines.append(GanttGenerator.head)
+    self.lines.append("project starts " + self.tree.getRoot().getRow().getStart())
+    self.nodeAsGantt(self.tree.getRoot())
+    self.lines.append(GanttGenerator.tail)
+    for l in self.lines :
+      print(l)
+
+  #----------------------------------------------------
+  def nodeAsGantt(self,node) :
+    self.lines.append(node.getRow().toGantt())
+    for c in node.getChildren() :
+      self.nodeAsGantt(c)
+
+
+#============================================
 class Row() :
   #----------------------------------------------------
   def __init__(self,row) :
@@ -70,13 +131,14 @@ class Row() :
     self.start=row['start'].strip() if row['start'] else '' 
     self.end=row['end'].strip() if row['end'] else ''
     self.who=row['who'].strip() if row['who'] else ''
-    self.statusValues={
-     'unknown' : 0,
-     'backlog' : 1,
-     'running' : 2,
-     'done'    : 3
-    }
-    status=row['status'].strip().lower() if row['status'] else ''
+    #self.statusValues={
+    # 'unknown' : 0,
+    # 'backlog' : 1,
+    # 'running' : 2,
+    # 'done'    : 3
+    #}
+    #status=row['status'].strip().lower() if row['status'] else ''
+    status=int(row['status'].strip()) if row['status'] else 0
     self.setStatus(status)
     #self.statusNum=self.statusValues[self.status] if self.status in self.statusValues else 0
     logging.warning("Created new Row <" + self.toString() +">")
@@ -100,6 +162,9 @@ class Row() :
   def getStatusNum(self) :
     return(self.statusNum)
   #----------------------------------------------------
+  def getStatusStr(self) :
+    return(self.statusStr)
+  #----------------------------------------------------
   def getWho(self) :
     return(self.who)
   #----------------------------------------------------
@@ -122,11 +187,20 @@ class Row() :
   #----------------------------------------------------
   def setStatus(self,status) :
     self.status=status
-    self.statusNum=self.statusValues[self.status] if self.status in self.statusValues else 0
+    if self.status < 0 :
+      self.statusStr="None"
+    elif self.status < 1 :
+      self.statusStr="Backlog"
+    elif self.status < 100 :
+      self.statusStr="Running"
+    else :
+      self.statusStr="Done"
+    self.statusNum=status
+    #self.statusNum=self.statusValues[self.status] if self.status in self.statusValues else 0
 
   #----------------------------------------------------
   def toString(self) :
-    return("{:s} {:20s} {:1s} {:12s} {:10s} {:10s} {:20s} {:8s} {:d} ".format(
+    return("{:s} {:20s} {:1s} {:12s} {:10s} {:10s} {:20s} {:3d} {:3d} ".format(
       self.getDepth(),
       self.getDesc(),
       self.getDirection(),
@@ -140,14 +214,25 @@ class Row() :
   #----------------------------------------------------
   def toWbs(self) :
     level=self.getDepth() + self.getDirection()
-    return("{:s} <b>{:s}</b>\\n{:s}\\n{:s}\\n{:s} <<{:s}>>".format(
+    return("{:s} <b>{:s}</b>\\n{:s}\\n{:s}\\n{:3d}%<<{:s}>>".format(
       level,
       self.getDesc(),
       self.start,
       self.end,
       self.status,
-      self.status,
+      self.statusStr,
       ))
+
+  #----------------------------------------------------
+  def toGantt(self) :
+    desc="[" + self.getDesc() + "]"
+    ganttLines=[]
+    if len(self.start) > 0 :
+      ganttLines.append(desc + " starts " + self.start)
+    if len(self.end) > 0 :
+      ganttLines.append(desc + " ends " + self.end)
+    
+    return("\n".join(ganttLines))
 
 #============================================
 class Node() :
@@ -156,6 +241,8 @@ class Node() :
     self.parent=parent
     self.children=[]
     self.row=row
+    self.upRow=None
+    self.downRow=None
     self.level=level
     logging.warning("Creating New node : row={:s} level={:d} ".format(self.row.toString(),self.level))
     if self.parent :
@@ -179,6 +266,18 @@ class Node() :
   def getRow(self) :
     return(self.row)
   #----------------------------------------------------
+  def getUpRow(self) :
+    return(self.upRow)
+  #----------------------------------------------------
+  def setUpRow(self) :
+    self.upRow=copy.deepcopy(self.row)
+  #----------------------------------------------------
+  def setDownRow(self) :
+    self.downRow=copy.deepcopy(self.row)
+  #----------------------------------------------------
+  def getDownRow(self) :
+    return(self.downRow)
+  #----------------------------------------------------
   def setParent(self,parent) :
     self.parent=parent
   #----------------------------------------------------
@@ -189,8 +288,18 @@ class Node() :
   def toString(self) :
     return("{:d} {:s}".format(
       self.getLevel(),
-      self.getRow().toString()
+      self.getRow().toString(),
     ))
+
+  #----------------------------------------------------
+  def toStringAll(self) :
+    return("{:d}\n{:s}\n{:s}\n{:s}".format(
+      self.getLevel(),
+      self.getRow().toString(),
+      self.getUpRow().toString(),
+      self.getDownRow().toString(),
+    ))
+
 
   #----------------------------------------------------
   def getDesc(self) :
@@ -319,10 +428,12 @@ class Percolator() :
     self.percolate(tree.getRoot())
     print("Final")
     self.display(tree.getRoot())
+    self.displayAll(tree.getRoot())
 
   #----------------------------------------------------
-  def parentToChild(self,parent,child) :
-
+  # status was character string
+  def XparentToChild(self,parent,child) :
+    child.setUpRow()
     if child.getRow().getStart() :
       if child.getRow().getStart() < parent.getRow().getStart() :
         logging.warning(child.getRow().getDesc() + " start date too small")
@@ -361,17 +472,95 @@ class Percolator() :
 
 
   #----------------------------------------------------
+  def parentToChild(self,parent,child) :
+    child.setUpRow()
+    if child.getRow().getStart() :
+      if child.getRow().getStart() < parent.getRow().getStart() :
+        logging.warning(child.getRow().getDesc() + " start date too small")
+        if self.args.fix :
+          logging.warning("Fixing")
+          child.getRow().setStart(parent.getRow().getStart())
+      else :
+        logging.debug(child.getRow().getDesc() + " start date coherent")
+    else :
+      logging.debug(child.getRow().getDesc() + " start not set")
+      child.getRow().setStart(parent.getRow().getStart())
+
+    if child.getRow().getEnd() :
+      if child.getRow().getEnd() > parent.getRow().getEnd() :
+        logging.warning(child.getRow().getDesc() + " end date too high")
+        if self.args.fix :
+          logging.warning("Fixing")
+          child.getRow().setEnd(parent.getRow().getEnd())
+      else :
+        logging.debug(child.getRow().getDesc() + " end date coherent")
+    else :
+      logging.debug(child.getRow().getDesc() + " end not set")
+      child.getRow().setEnd(parent.getRow().getEnd())
+
+    if child.getRow().getStatus() :
+      if ((child.getRow().getStatus() >= 0) and (child.getRow().getStatus() < 100))  and   parent.getRow().getStatus() >= 100 :
+        logging.warning(child.getRow().getDesc() + " status " + str(child.getRow().getStatus()) + " too high " + str(parent.getRow().getStatus()))
+        if self.args.fix :
+          logging.warning("Fixing")
+          child.getRow().setStatus(parent.getRow().getStatus())
+      else :
+        logging.debug(child.getRow().getDesc() + " status coherent")
+    else :
+      logging.debug(child.getRow().getDesc() + " status not set")
+      child.getRow().setStatus(parent.getRow().getStatus())
+
+  #----------------------------------------------------
+  def parentToChildAll(self,parent,child) :
+    parent.setUpRow()
+    child.setUpRow()
+    if not child.getUpRow().getStart() :
+      logging.debug(child.getUpRow().getDesc() + " start not set")
+      child.getUpRow().setStart(parent.getUpRow().getStart())
+    if not child.getUpRow().getEnd() :
+      logging.debug(child.getUpRow().getDesc() + " end not set")
+      child.getUpRow().setEnd(parent.getUpRow().getEnd())
+    if not child.getUpRow().getStatus() :
+      logging.debug(child.getUpRow().getDesc() + " status not set")
+      child.getUpRow().setStatus(parent.getUpRow().getStatus())
+
+  #----------------------------------------------------
+  def childToParentAll(self,parent,child) :
+    parent.setDownRow()
+    child.setDownRow()
+    if not parent.getDownRow().getStart() :
+      logging.debug(parent.getDownRow().getDesc() + " start not set")
+      parent.getDownRow().setStart(child.getDownRow().getStart())
+    if not parent.getDownRow().getEnd() :
+      logging.debug(parent.getDownRow().getDesc() + " end not set")
+      parent.getDownRow().setEnd(child.getUpRow().getEnd())
+    if not parent.getDownRow().getStatus() :
+      logging.debug(parent.getDownRow().getDesc() + " status not set")
+      parent.getDownRow().setStatus(child.getDownRow().getStatus())
+
+
+  #----------------------------------------------------
   def display(self,node) :
     print(node.toString())
     for c in node.getChildren() :
       self.display(c)
 
+
+  #----------------------------------------------------
+  def displayAll(self,node) :
+    print(node.toStringAll())
+    for c in node.getChildren() :
+      self.displayAll(c)
+
+
   #----------------------------------------------------
   def percolate(self,node) :
     logging.debug("before " + node.toString())
     for c in node.getChildren() :
+      self.parentToChildAll(node,c)
       self.parentToChild(node,c)
       self.percolate(c)
+      self.childToParentAll(node,c)
     logging.debug("after  " + node.toString())
 
 #----------------------------------------------------
@@ -399,6 +588,7 @@ def fScan(args) :
   build(args.file,tree)
   Percolator(args,tree)
   WbsGenerator(args,tree)
+  GanttGenerator(args,tree)
 
 #----------------------------------------------------
 parser = argparse.ArgumentParser()
